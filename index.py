73eaf6985de79510a6b536ff86e5909746f8d2c2
Samiullah Hashmi <samiullahhashmi27@gmail.com>
2024-04-13 15:13:27 +0500

    Day 12

---
+++ index.py
@@ -0,0 +1,52 @@
+# Number Guessing Game
+# FINAL PROJECT
+
+import random
+from art import logo
+
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
+turns = 0
+
+def check_answer(guess, answer, turns):
+    if guess > answer:
+        print("Too High")
+        return turns - 1
+    
+    elif guess < answer:
+        print("Too Low")
+        return turns - 1
+    
+    else: 
+        print(f"You got it! The answer was {answer}")
+
+def set_difficulty():
+    level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    if level == 'easy':
+        return EASY_LEVEL_TURNS
+    else:
+        return HARD_LEVEL_TURNS
+    
+
+def game():
+    print(logo)
+    print("Welcome to the Number Guessing Game!\n")
+    print("I'm thinking of a number between 1 and 100")
+    answer = random.randint(1,100)
+    turns = set_difficulty()
+
+    guess = 0
+    while guess != answer:
+        print(f"You have {turns} attemps remaining to guess the unknown number.")
+        guess = int(input("Make a guess: "))
+        turns = check_answer(guess, answer, turns)
+
+        if turns == 0:
+            print("You've run out of guesses, you lose")
+            return 
+        elif guess != answer:
+            print("Guess again.")
+
+game()
+
